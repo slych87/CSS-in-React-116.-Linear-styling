aee1e91052762402b91ce401af7a0139529df103
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-18 17:31:18 +0200

    change with if()

--- app/src/App.js
+++ app/src/App.js
@@ -30,32 +30,46 @@ class App extends Component {
     //   color: 'white',
     // }
 
-    const styles = {
-      btn_off: {
-        border: '2px solid black',
-        padding: "10px 20px",
-        fontFamily: 'arial',
-        fontSize: 30,
-        display: 'block',
-        margin: '20px auto',
-        backgroundColor: 'white',
-      },
+    // const styles = {
+    //   btn_off: {
+    //     border: '2px solid black',
+    //     padding: "10px 20px",
+    //     fontFamily: 'arial',
+    //     fontSize: 30,
+    //     display: 'block',
+    //     margin: '20px auto',
+    //     backgroundColor: 'white',
+    //   },
 
-      btn_on: {
-        border: '2px solid black',
-        padding: "10px 20px",
-        fontFamily: 'arial',
-        fontSize: 30,
-        display: 'block',
-        margin: '20px auto',
-        backgroundColor: 'cadetblue',
-        color: 'white',
-      }
+    //   btn_on: {
+    //     border: '2px solid black',
+    //     padding: "10px 20px",
+    //     fontFamily: 'arial',
+    //     fontSize: 30,
+    //     display: 'block',
+    //     margin: '20px auto',
+    //     backgroundColor: 'cadetblue',
+    //     color: 'white',
+    //   }
+    // }
+
+    let btn = {
+      border: '2px solid black',
+      padding: "10px 20px",
+      fontFamily: 'arial',
+      fontSize: 30,
+      display: 'block',
+      margin: '20px auto',
+      backgroundColor: 'white',
+    }
+    if (this.state.active) {
+      btn.backgroundColor = 'cadetblue';
+      btn.color = 'white';
     }
 
     return (
       <div>
-        <button style={this.state.active ? styles.btn_on : styles.btn_off} onClick={() => this.setState({ active: !this.state.active })}>{this.state.active ? "Wyłącz" : "Włącz"}</button>
+        <button style={btn} onClick={() => this.setState({ active: !this.state.active })}>{this.state.active ? "Wyłącz" : "Włącz"}</button>
       </div>
     );
   }
