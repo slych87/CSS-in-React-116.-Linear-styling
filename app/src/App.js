0e1f6498cf9972e97444686f3870f71b85bb9e3d
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-18 17:14:23 +0200

    In one object as a variable.

--- app/src/App.js
+++ app/src/App.js
@@ -6,10 +6,56 @@ class App extends Component {
     active: false
   }
 
+
   render() {
+
+    // const btn_off = {
+    //   border: '2px solid black',
+    //   padding: "10px 20px",
+    //   fontFamily: 'arial',
+    //   fontSize: 30,
+    //   display: 'block',
+    //   margin: '20px auto',
+    //   backgroundColor: 'white',
+    // }
+
+    // const btn_on = {
+    //   border: '2px solid black',
+    //   padding: "10px 20px",
+    //   fontFamily: 'arial',
+    //   fontSize: 30,
+    //   display: 'block',
+    //   margin: '20px auto',
+    //   backgroundColor: 'cadetblue',
+    //   color: 'white',
+    // }
+
+    const styles = {
+      btn_off: {
+        border: '2px solid black',
+        padding: "10px 20px",
+        fontFamily: 'arial',
+        fontSize: 30,
+        display: 'block',
+        margin: '20px auto',
+        backgroundColor: 'white',
+      },
+
+      btn_on: {
+        border: '2px solid black',
+        padding: "10px 20px",
+        fontFamily: 'arial',
+        fontSize: 30,
+        display: 'block',
+        margin: '20px auto',
+        backgroundColor: 'cadetblue',
+        color: 'white',
+      }
+    }
+
     return (
       <div>
-        <button style={{ border: '4px solid red', padding: 20 }} onClick={() => this.setState({ active: !this.state.active })}>{this.state.active ? "Wyłącz" : "Włącz"}</button>
+        <button style={this.state.active ? styles.btn_on : styles.btn_off} onClick={() => this.setState({ active: !this.state.active })}>{this.state.active ? "Wyłącz" : "Włącz"}</button>
       </div>
     );
   }
