4c2728c6a7da2b1e17464ece7e9a6692eb7a8f1d
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-18 17:48:30 +0200

    CSS from an external file

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import "./App.css";
+import Text from './Text';
 
 class App extends Component {
 
@@ -9,67 +11,11 @@ class App extends Component {
 
   render() {
 
-    // const btn_off = {
-    //   border: '2px solid black',
-    //   padding: "10px 20px",
-    //   fontFamily: 'arial',
-    //   fontSize: 30,
-    //   display: 'block',
-    //   margin: '20px auto',
-    //   backgroundColor: 'white',
-    // }
-
-    // const btn_on = {
-    //   border: '2px solid black',
-    //   padding: "10px 20px",
-    //   fontFamily: 'arial',
-    //   fontSize: 30,
-    //   display: 'block',
-    //   margin: '20px auto',
-    //   backgroundColor: 'cadetblue',
-    //   color: 'white',
-    // }
-
-    // const styles = {
-    //   btn_off: {
-    //     border: '2px solid black',
-    //     padding: "10px 20px",
-    //     fontFamily: 'arial',
-    //     fontSize: 30,
-    //     display: 'block',
-    //     margin: '20px auto',
-    //     backgroundColor: 'white',
-    //   },
-
-    //   btn_on: {
-    //     border: '2px solid black',
-    //     padding: "10px 20px",
-    //     fontFamily: 'arial',
-    //     fontSize: 30,
-    //     display: 'block',
-    //     margin: '20px auto',
-    //     backgroundColor: 'cadetblue',
-    //     color: 'white',
-    //   }
-    // }
-
-    let btn = {
-      border: '2px solid black',
-      padding: "10px 20px",
-      fontFamily: 'arial',
-      fontSize: 30,
-      display: 'block',
-      margin: '20px auto',
-      backgroundColor: 'white',
-    }
-    if (this.state.active) {
-      btn.backgroundColor = 'cadetblue';
-      btn.color = 'white';
-    }
 
     return (
-      <div>
-        <button style={btn} onClick={() => this.setState({ active: !this.state.active })}>{this.state.active ? "Wyłącz" : "Włącz"}</button>
+      <div className="app">
+        <h1>Witaj nastronie</h1>
+        <Text />
       </div>
     );
   }
---
+++ app/src/Text.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Text.css"
+
+const Text = () => {
+    return (
+        <p className="red">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt nesciunt autem cumque ad eos officia fuga molestias error enim. Reiciendis sapiente fugit quaerat asperiores inventore provident quo molestias libero corrupti?
+        </p>
+    );
+}
+
+export default Text;
