4e0829e5a743c05997889c053edc7981717944e8
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-18 18:10:20 +0200

    use of arrays in styles

--- app/src/App.js
+++ app/src/App.js
@@ -5,16 +5,25 @@ import Text from './Text';
 class App extends Component {
 
   state = {
-    active: false
+
+    underline: false
   }
 
 
   render() {
+    const text = "Witaj na stronie";
+    // let classes = "";
+    // if (this.state.underline) classes += " line";
+    // if (text.length > 10) classes += " big"
 
+    const classes = ['one'];
+    if (this.state.underline) classes.push('line');
+    if (text.length > 10) classes.push('big')
 
     return (
-      <div className="app">
-        <h1>Witaj nastronie</h1>
+      <div className="app" onClick={() => this.setState({ underline: !this.state.underline })}>
+
+        <h1 className={classes.join(" ")}>Witaj nastronie</h1>
         <Text />
       </div>
     );
